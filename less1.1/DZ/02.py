25e1b89e668dba20ae733e1e41bff89e28e8bcd4
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-09-24 12:56:06 +0300

    исправление дз 2

--- less1.1/DZ/02.py
+++ less1.1/DZ/02.py
@@ -3,4 +3,6 @@
 for x in range(2):
         for y in range(2):
             for z in range(2):
-                print(not (x or y or z) == (not x and not y and not z), x, y, z)
+                if (not (x or y or z) == (not x and not y and not z)):
+                    print(x, y, z)
+                
