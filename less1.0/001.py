cb5bb0cad71699704b499d433ef5600ba106ec62
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-09-24 10:11:02 +0300

    первый урок

---
+++ less1.0/001.py
@@ -0,0 +1,23 @@
+print('hello world')
+
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value=12333
+print(type(value))
+s = "hello world"
+print(s) # вывод строки
+print(a, ' - ', b, ' - ',s)
+print('{} - {} - {}'.format(a, b,s))
+
+f= True
+print(f)
+
+
+List = ['1', '2', '3', 'hello', 1, 2,3, 4.5, True]
+print(List)
+
+print(f"{f=}")
